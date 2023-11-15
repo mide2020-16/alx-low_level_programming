cb148f49f7c62191ff8998760e1b269365a8305e
mide2020-16 <efuwapeayomide51@gmail.com>
2023-11-15 06:43:33 -0800

    recursu=ion solved

--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -8,8 +8,8 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-char _putchar(char c)
+int _putchar(char c)
 {
-	return (write(&c));
+	return (write(1, &c, 1));
 }
 
