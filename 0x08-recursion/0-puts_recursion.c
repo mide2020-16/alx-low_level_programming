1d440458365dcfdcc8f3f87cb7c4a5cdd315adfc
mide2020-16 <efuwapeayomide51@gmail.com>
2023-11-15 06:53:58 -0800

    recursu=ion solved

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,15 +6,16 @@
  * Return: Always Success
  */
 
-void _puts_recursion(char *s)
+void _puts_recursion(c)
 {
-	_putchar(*s);
-
-	_puts_recursion(s + 1);
 
 	if (*s == '\0')
 	{
 		_putchar('\n');
 
 	}
+	_putchar(*s);
+
+	_puts_recursion(s + 1);
+
 }
