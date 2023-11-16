387bb6ca5e768bdfbcfd9478bc340e24c14bad21
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2023-11-16 15:11:39 +0100

    gh correction of 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,29 +1,5 @@
 #include "main.h"
 
-/**
- * _sqrt_recursion - Fins=ds the square root of a number.
- *
- * @n: the number.
- *
- * Return: Always Success
- */
-
-int _sqrt_recursion(int n)
-{
-	if ((n != 0 && n == (n / 2) * (n / 2)) || n < 0)
-	{
-		return (-1);
-
-	}
-	if (n == 0 || n == 1)
-	{
-		return (n);
-
-	}
-	return (_binary_search(n, 0, n));
-
-
-}
 
 /**
  * _binary_search - binary search function(recursive)
@@ -58,4 +34,28 @@ int _binary_search(int n, int start, int end)
 	{
 		return (_binary_search(n, start, mid - 1));
 	}
+}
+/**
+ * _sqrt_recursion - Fins=ds the square root of a number.
+ *
+ * @n: the number.
+ *
+ * Return: Always Success
+ */
+
+int _sqrt_recursion(int n)
+{
+	if ((n != 0 && n == (n / 2) * (n / 2)) || n < 0)
+	{
+		return (-1);
+
+	}
+	if (n == 0 || n == 1)
+	{
+		return (n);
+
+	}
+	return (_binary_search(n, 0, n));
+
+
 }
