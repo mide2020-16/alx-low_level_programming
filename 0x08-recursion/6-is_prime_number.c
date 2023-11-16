f9f4d2d2b3e6f7758ebf7a59d0b91ed4790a7c4f
mide2020-16 <efuwapeayomide51@gmail.com>
2023-11-16 12:53:57 -0800

    recursion 6 solved

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _prime_checker - checks for a prime number
+ * @n: the number
+ * @div: the divisor
+ * Return: n Always
+ */
+
+int _prime_checker(int n, int div)
+{
+	if (n <= 1 || n % div == 0)
+	{
+		return (0);
+	}
+
+	if (div * div > n)
+	{
+		return (1);
+	}
+
+	return (_prime_checker(n, div + 1));
+}
+
+/**
+ * is_prime_number - To know if a number is a prime numer
+ * @n: The number to check
+ * Return: returns 0 for failure and 1 for success
+ */
+
+int is_prime_number(int n)
+{
+	_prime_checker(n, 2);
+}
