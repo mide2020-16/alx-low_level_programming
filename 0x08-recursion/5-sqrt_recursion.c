c848c35baa15313c378072c6ae366932493dff91
mide2020-16 <efuwapeayomide51@gmail.com>
2023-11-16 06:02:49 -0800

    recursion5 solved

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Fins=ds the square root of a number.
+ *
+ * @n: the number.
+ *
+ * Return: Always Success
+ */
+
+int _sqrt_recursion(int n)
+{
+	if ((n != 0 && n == (n / 2) * (n / 2)) || n < 0)
+	{
+		return (-1);
+
+	}
+	if (n == 0 || n == 1)
+	{
+		return (n);
+
+	}
+	return (_binary_search(n, 0, n));
+
+
+}
+
+/**
+ * _binary_search - binary search function(recursive)
+ * @n: the number to find the squae root of
+ * @start: starting point of search range
+ * @end: ending point of search range
+ * Return: success ALways
+ */
+
+int _binary_search(int n, int start, int end)
+{
+	if (start > end)
+	{
+		return (-1);
+
+	}
+
+	int mid = start + (end - start);
+
+	if (mid * mid == n)
+	{
+		return (mid);
+
+	}
+
+	if (mid * mid < n)
+	{
+		return (_binay_search(n, mid + 1, end));
+	}
+
+	if (mid * mid > n)
+	{
+		return (_binary_search(n, start, mid - 1));
+	}
+}
