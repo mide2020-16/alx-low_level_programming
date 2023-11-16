ed6a7a7ba826663b4fea8964de759b6766bac71f
mide2020-16 <efuwapeayomide51@gmail.com>
2023-11-16 04:01:28 -0800

    recursion 2 solved

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/*
+ * _print_rev_recursion(char *s) - This prints a string in reverse order.
+ * @s: the string to be printed
+ * return: Always Successz
+ */
+
+void _print_rev_recursion(char *s)
+{
+	/*
+	 * Check if the string as reached the null terminator
+	 */
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+
+	_putchar(*s);
+
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,6 +5,9 @@
 #include <unistd.h>
 
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 char _putchar(char c);
+
+
 
 #endif
