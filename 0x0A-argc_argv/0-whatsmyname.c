6fa9242818be156659ce86abfe1371dc8ec44901
mide2020-16 <efuwapeayomide51@gmail.com>
2023-11-17 23:50:29 -0800

    argc 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * char main - A program tha prnts its name followed by anew line
+ *
+ * @argc: count arg
+ *
+ * @argv: vector arg
+ *
+ * Return: Name Success
+ */
+
+char main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc]);
+
+	return (0);
+}
