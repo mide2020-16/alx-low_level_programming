a0e5f8a100ddfdf709cb8ff3a2b3bae2ae15e93b
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2023-11-19 15:14:13 +0100

    n-r

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,75 +1,38 @@
 #include "main.h"
 #include <math.h>
 
-
 /**
- * _binary_search - binary search function(recursive)
- * @n: the number to find the squae root of
- * @start: starting point of search range
- * @end: ending point of search range
- * Return: success ALways
+ * binary_search_recursive - recursive helper function for binary search
+ * @n: the number to find the square root of
+ * @guess: current guess for the square root
+ * @epsilon: the desired level of accuracy
+ * Return: the square root of n or -1 if n is negative
  */
-
-int _binary_search(int n, double espilon)
+double binary_search_recursive(int n, double guess, double epsilon)
 {
-	/* This is called Newton-Raphson Iterative Method. I saw this in Mathematics Textbook
- 	and i asked chtgpt to explain 
-  	*/
-	int guess;
-	guess = n /2;
-
-	while ((guess * guess - n) > espilon)
-		{
-			guess = 0.5 * (guess + (guess / n));
-		}
-	return guess;
-	/*int mid;
-
-	if (start > end)
-	{
-		return (-1);
-
-	}
-
-	mid = (start + end) / 2;
-
-	if (mid * mid == n)
-	{
-		return (mid);
-
-	}
-
-	if (mid * mid < n)
-	{
-		return (_binary_search(n, mid + 1, end));
-	}
-
-	return (_binary_search(n, start, mid - 1));
- 	*/
-
+    if (fabs(guess * guess - n) <= epsilon)
+    {
+        return guess;
+    }
+    else
+    {
+        return binary_search_recursive(n, 0.5 * (guess + n / guess), epsilon);
+    }
 }
-/**
- * _sqrt_recursion - Fins=ds the square root of a number.
- *
- * @n: the number.
- *
- * Return: Always Success
- */
 
+/**
+ * _sqrt_recursion - Finds the square root of a number.
+ * @n: the number.
+ * Return: the square root of n or -1 if n is negative
+ */
 int _sqrt_recursion(int n)
 {
-	double espilon = 0.000000000001;
-	if (n < 0)
-	{
-		return (-1);
+    double epsilon = 0.000000000001;
 
-	}
-	if (n == 0 || n == 1)
-	{
-		return (n);
+    if (n < 0)
+    {
+        return -1;
+    }
 
-	}
-	return (_binary_search(n, espilon));
-	
-
+    return binary_search_recursive(n, n / 2.0, epsilon);
 }
