42870d4597f39762a75076740c048fcd7de34b3d
mide2020-16 <efuwapeayomide51@gmail.com>
2023-11-20 07:15:21 -0800

    update 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - all arguments
+ * @argc: count
+ * @argv: vector
+ * Return: Success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0;i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
