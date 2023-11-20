79cf30a34a70327600552704b94b00060d499d6c
mide2020-16 <efuwapeayomide51@gmail.com>
2023-11-20 08:02:02 -0800

    mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,16 +9,15 @@ include "main.h"
 
 int main(int argc, char *argv[])
 {
-	int i;
 
-	if (argc < 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	ans = 1;
-	ans = argv[1] * argv[2];
+	int ans;
+	ans = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", ans);
 	return (0);
 }
