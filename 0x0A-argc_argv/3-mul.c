84d062e073e617c4923a5ad17cbfeb45a2647913
mide2020-16 <efuwapeayomide51@gmail.com>
2023-11-22 13:24:45 -0800

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,6 @@
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
  * main - multiplicatin
  * @argc: counter
@@ -14,12 +14,17 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		printf("Error\n");
+
 		return (1);
 	}
 
 	int num1 = atoi(argv[1]);
+
 	int num2 = atoi(argv[2]);
+
 	int ans = num1 * num2;
+
 	printf("%d\n", ans);
+
 	return (0);
 }
