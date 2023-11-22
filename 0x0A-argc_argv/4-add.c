53f27f872f5b2eda9ce830c950caf705d2e974e4
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2023-11-22 23:26:34 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,57 +4,33 @@
 
 /**
  * main - addtion of numbers
- *
  * @argc: the counts
- *
  * @argv: the vector
- *
  * Return: Success
  */
-
 int main(int argc, char *argv[])
 {
-	int result = 0;
-
-	int i;
-
+	int i, result = 0;
 	if (argc == 1)
 	{
 		printf("0\n");
-
 		return (0);
-
 	}
-	
 	for (i = 1; i < argc; i++)
 	{
-
 		int j;
-
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			
 			if (!isdigit(argv[i][j]))
 			{
-				
 				printf("Error\n");
-
 				return (1);
-
 			}
-
 		}
-
 		int num;
-
 		num = atoi(argv[i]);
-
 		result += num;
-
 	}
-
 	printf("%d\n", result);
-
 	return (0);
 }
-
