94aa22306e2a4db90bcd9a7d2b3543dcd31fea81
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2023-11-22 22:41:26 +0100

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -24,8 +24,10 @@ int main(int argc, char *argv[])
 
 	int ans = num1 * num2;
 
-	if ((num1 == 0 || num2 == 0) && (*argv[1] != '0' || *argv[2] != '0'))
+	if ((num1 == 0 && *argv[1] != '0') || (num2 == 0 && *argv[2] != '0'))
 	{
+		printf("Error\n");
+		
 		return (1);
 	}
 	printf("%d\n", ans);
