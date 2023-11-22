b90d4616b8a759e82589a06364219843ad2c115e
mide2020-16 <efuwapeayomide51@gmail.com>
2023-11-22 14:11:44 -0800

    adds

---
+++ 0x0A-argc_argv/4-adds.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main - addtion of numbers
+ *
+ * @argc: the counts
+ *
+ * @argv: the vector
+ *
+ * Return: Success
+ */
+
+int main(int argc, char *argv[])
+{
+	int result = 0;
+
+	int i;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+
+		return (0);
+
+	}
+	
+	for (i = 1; i < argc; i++)
+	{
+
+		int j;
+
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			
+			if (!isdigit(argv[i][j]))
+			{
+				
+				printf("Error\n");
+
+				return (1);
+
+			}
+
+		}
+
+		int num;
+
+		num = atoi(argv[i]);
+
+		result += num;
+
+	}
+
+	printf(result);
+
+	return (0);
+
+
