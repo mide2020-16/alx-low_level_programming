70c3c1ca365a65728dbb69529b75d1c5f6fa9991
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2023-11-23 23:40:14 +0100

    Create 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *_strdup - duplicates content
+ * @str: the string
+ * Return: Success
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+	
+	if (str == NULL)
+		return (NULL);
+
+	s = malloc(strlen(str) + 1);
+
+	if (s != NULL)
+		strcpy(s, str);
+
+	return (s);
+}
