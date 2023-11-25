3a8cf6ab08621617c82df70f3874180a8c3b6b70
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2023-11-25 10:21:12 +0100

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,16 +13,17 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *mem;
 	int total_size;
+
 	total_size = nmemb * size;
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	
+
 	mem = malloc(total_size);
-	
+
 	if (mem == NULL)
 		return (NULL);
-	
+
 	memset(mem, 0, total_size);
-	
+
 	return (mem);
 }
