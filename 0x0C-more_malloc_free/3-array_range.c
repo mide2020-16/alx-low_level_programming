fa78e8cf95f2ecf5c1635e57561a00ce9f000e6c
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2023-11-25 11:52:33 +0100

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,18 +16,18 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	
+
 	total_size = (max - min) + 1;
-	
+
 	arr = malloc(total_size * sizeof(int));
 
 	if (arr == NULL)
 		return (NULL);
-	
+
 	arr[0] = min;
 	for (i = 1; i < total_size; ++i)
-		{
-			arr[i] = arr[i - 1] + 1;
-		}
+	{
+		arr[i] = arr[i - 1] + 1;
+	}
 	return (arr);
 }
