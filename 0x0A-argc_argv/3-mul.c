5fa362f5d394c6e224a93c2d94a4b519875dfcc9
mide2020-16 <efuwapeayomide51@gmail.com>
2023-11-25 13:12:22 -0800

    merge

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -24,12 +24,6 @@ int main(int argc, char *argv[])
 
 	int ans = num1 * num2;
 
-	if ((num1 == 0 && *argv[1] != '0') || (num2 == 0 && *argv[2] != '0'))
-	{
-		printf("Error\n");
-		
-		return (1);
-	}
 	printf("%d\n", ans);
 
 	return (0);
