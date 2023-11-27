f8c06803874e36e2fdd7940385eed55ddb8f6e7b
mide2020-16 <efuwapeayomide51@gmail.com>
2023-11-27 05:30:04 -0800

    done with 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file it was compiled form.
+ *
+ * Return: Success (Always)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
