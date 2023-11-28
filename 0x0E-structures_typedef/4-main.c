55f02d13d75c71d2d967a7d622b3f75dd7f3b5c5
mide2020-16 <efuwapeayomide51@gmail.com>
2023-11-28 15:33:09 -0800

    ignored files

--- 0x0E-structures_typedef/4-main.c
+++ 0x0E-structures_typedef/4-main.c
@@ -10,7 +10,7 @@ int main(void)
 {
     dog_t *my_dog;
 
-    my_dog = new_dog("Poppy", 3.5, "Bob");
+    my_dog = new_dog("Ayomide", 6.0, "AyomideOreoluwa");
     printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
     return (0);
 }
