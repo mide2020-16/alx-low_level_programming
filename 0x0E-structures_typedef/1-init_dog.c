7789821b9dec9720e4a99b408a4fe148c1c80bfb
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2023-11-28 22:42:21 +0100

    Create 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - The new dog initialization
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+  if (d == NULL)
+	  return;
+  d->name = name;
+  d->age = age;
+  d->owner = owner;
+}
