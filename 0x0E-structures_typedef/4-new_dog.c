7ad9b2c17f7aea73dd1925c888af555602f7bea6
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2023-11-29 00:32:12 +0100

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,7 +4,7 @@
 
 /**
  * new_dog - The new dog struct initialzation
- * @name: nName
+ * @name: Name
  * @age: Age
  * @owner: Owner
  * Return: A new dog
@@ -17,8 +17,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int name_len;
 	int owner_len;
 
-	name_len = strlen(name);
-	owner_len = strlen(owner);
+	name_len = strlen(name) * sizeof(char) + 1;
+	owner_len = strlen(owner) * sizeof(char) + 1;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
@@ -30,19 +30,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	}
-	dog->name = malloc(sizeof(char) * name_len + 1);
+	dog->name = malloc(name_len);
 
 	if ((dog->name) == NULL)
 	{
-		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
-	dog->name = name;
+	strcpy(dog->name, name);
 
 	dog->age = age;
 
-	dog->owner = malloc(sizeof(char) * owner_len + 1);
+	dog->owner = malloc(owner_len);
 
 	if (dog->owner == NULL)
 	{
@@ -50,6 +49,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog);
 		return (NULL);
 	}
-	dog->owner = owner;
+	strcpy(dog->owner = owner);
 	return (dog);
 }
