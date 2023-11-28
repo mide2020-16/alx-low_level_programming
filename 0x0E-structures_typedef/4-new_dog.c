078de5b6242569da1dccddb9cbcd10ba158db3c1
mide2020-16 <efuwapeayomide51@gmail.com>
2023-11-28 15:05:08 -0800

    done with 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,33 +2,47 @@
 #include <stdlib.h>
 
 /**
- * dog_t new_dog - The new dog struct initialzation
+ * new_dog - The new dog struct initialzation
  * @name: nName
  * @age: Age
  * @owner: Owner
- * Description: A new dog
+ * Return: A new dog
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 
-	dog = malloc(sizeof(dog_t));
+	if (name == NULL || owner == NULL)
+		return (NULL);
 
-	if (dog != NULL)
-	{
-		if (name == NULL || owner == NULL)
-			return (NULL);
-		else
-		{
-			dog->name = name;
-			dog->age = age;
-			dog->owner = owner;
-		}
-	}
-	else 
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
 	{
 		free(dog);
 		return (NULL);
+
 	}
+	dog->name = malloc(sizeof(char) * strlen(name) + 1);
+
+	if ((dog->name) == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	dog->name = name;
+
+	dog->age = age;
+
+	dog->owner = malloc(sizeof(char) * strlen(owner) + 1);
+
+	if (dog->owner == NULL)
+	{
+		free(dog->owner);
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = owner;
+	return (dog);
 }
