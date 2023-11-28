9413110e2285dbb49b067acfded1ed3db05799ea
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2023-11-28 16:59:40 +0100

    Addd

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,19 +9,23 @@
  * @age: age
  * @owner: owner
  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL && name != NULL && owner != NULL)
 	{
-		d->name = name;
-		d->owner = owner;
+		d->name = strdup(name);
+		d->owner = strdup(owner);
 
 		if (d->name != NULL && d->owner != NULL)
+		{
 			d->age = age;
+		}
 		else
 		{
 			free(d->name);
 			free(d->owner);
+
 			d->name = NULL;
 			d->owner = NULL;
 		}
