8d636516a18e9d5dda02dda5e566e99fc8844515
mide2020-16 <efuwapeayomide51@gmail.com>
2023-11-28 15:58:28 -0800

    done with 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees memory allocated to struct dog
+ * @d: dog instances
+ * Return: dog
+ */
+
+void free_dog(dog_t *d)
+{
+	d = malloc(sizeof(dog_t));
+
+	if (d == NULL)
+	{
+		return;
+	}
+	free(d->name);
+	free(d->owner);
+	free(d);
+
+}
