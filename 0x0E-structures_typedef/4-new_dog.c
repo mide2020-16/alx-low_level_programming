bdf37bb85408bef18a1749e273838ea94d1c78df
mide2020-16 <efuwapeayomide51@gmail.com>
2023-11-28 15:18:31 -0800

    checks

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * new_dog - The new dog struct initialzation
@@ -13,6 +14,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 
+	int name_len;
+	int owner_len;
+
+	name_len = strlen(name);
+	owner_len = strlen(owner);
+
 	if (name == NULL || owner == NULL)
 		return (NULL);
 
@@ -23,7 +30,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	}
-	dog->name = malloc(sizeof(char) * strlen(name) + 1);
+	dog->name = malloc(sizeof(char) * name_len + 1);
 
 	if ((dog->name) == NULL)
 	{
@@ -35,7 +42,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog->age = age;
 
-	dog->owner = malloc(sizeof(char) * strlen(owner) + 1);
+	dog->owner = malloc(sizeof(char) * owner_len + 1);
 
 	if (dog->owner == NULL)
 	{
