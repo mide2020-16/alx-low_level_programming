f850ef8f80b7487ca4e88f87e702fc1c8a68a250
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2023-11-28 22:25:34 +0100

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,7 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 /**
  * init_dog - Initialization of a dog
@@ -13,37 +12,12 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL && name != NULL && owner != NULL)
-	{
-		d->name = strdup(name);
-		d->owner = strdup(owner);
+	d = malloc(sizeof(struct dog));
 
-		if (d->name != NULL && d->owner != NULL)
-		{
-			d->age = age;
-		}
-		else
-		{
-			free(d->name);
-			free(d->owner);
+	if (d == NULL)
+		return (NULL);
 
-			d->name = NULL;
-			d->owner = NULL;
-		}
-	}
-}
-
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    struct dog my_dog;
-
-    init_dog(&my_dog, "Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
+	d->name = name;
+	d->owner = owner;
+	d->age = age;
 }
