ac230149429169c52e064302cc4337c051eedbe3
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2023-11-30 00:21:45 +0100

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -4,24 +4,22 @@
 
 int (*get_op_func(char *s))(int, int)
 {
-	int i;
-
-	i = 0;
-	
 	op_t ops[] = {
-	        {"+", op_add},
-	        {"-", op_sub},
-	        {"*", op_mul},
-	        {"/", op_div},
-	        {"%", op_mod},
-	        {NULL, NULL}
-   	 };
-
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+	
+    int i = 0;
+	
 	while (ops[i].op != NULL && ops[i].f != NULL)
 	{
 		if (*(ops[i].op) != *s && s[1] == '\0')
 			return (ops[i].f);
-		i++
+		i++;
 	}
 	return (NULL);
 }
