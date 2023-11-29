9a06f245937254418d706fedbd01b6be1a698434
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2023-11-30 00:30:21 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -5,24 +5,19 @@
 int main(int argc, char *argv[])
 {
 	int i, num1, num2, result;
-
-	char *symbol;
-
-	i = 0;
-	
-	num1 = atoi(argv[1]);
-	symbol = argv[2];
-	num2 = atoi(argv[3]);
 	
 	int (*operator)(int, int);
-
-	operator = get_op_func(symbol)
-
+	
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	operator = get_op_func(argv[2]);
 
 	if (operator == NULL)
 	{
@@ -30,12 +25,14 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	if ((symbol == '/' && num2 == 0) || (symbol == '%' && num2 == 0))
+	if ((argv[2] == '/' || argv[2] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
+	
 	result = operator(num1, num2);
+	
 	printf("%d\n", result);
 
 	return (0);
