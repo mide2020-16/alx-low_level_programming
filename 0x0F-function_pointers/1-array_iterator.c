244a526b42ea9df688f7394be76e07b1f5e2b9fc
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2023-11-29 14:28:32 +0100

    Create 1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Iterates through an array
+ * @array: The array
+ * @size: Size of the array
+ * @action: pointer to function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+  
+}
