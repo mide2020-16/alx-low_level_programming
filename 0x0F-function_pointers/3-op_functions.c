899bf9adb4032e34e9172c258ce5b0c9d7e3f65e
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2023-11-29 15:00:52 +0100

    Create 3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Addition of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: Success
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Subtraction of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: Success
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplication of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: Success
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_div - Division of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: Success
+ */
+
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Modulus of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: Success
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
