3e276616abedbb294e71a0626ef0114c79fbe7e3
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2023-11-29 14:35:40 +0100

    Create 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,13 @@
+#include "function_pointer.h"
+
+/**
+ * int_index - Validates a value
+ * @array: The array
+ * @size: size of the array
+ * @cmp: pointer to compare function
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+  
+}
