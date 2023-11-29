5f6402557654b46a28f5545803a83cb375e1e197
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2023-11-29 14:20:42 +0100

    Create 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a Name
+ * @name: Name
+ * @f: pointer to a function that prints Name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	
+}
