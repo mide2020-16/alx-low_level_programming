e62f1a37a44c93c7e4e97f12682dc5729836ab65
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2023-11-30 02:46:44 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -26,13 +26,13 @@ int main(int argc, char *argv[])
 
 	operator = get_op_func(argv[2]);
 
-	if (operator == NULL)
+	if (operator == NULL || (argv[2])[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	if ((*argv[2] == '/' && num2 == 0) || (*argv[2] == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
