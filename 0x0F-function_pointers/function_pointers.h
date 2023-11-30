da5aa5ffaa868e5d776ab70790cfa3ae7928e3dd
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2023-11-30 01:40:37 +0100

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,8 @@
-#include <stddef.h>
-#include <stdio.h>
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+
+#include <stddef.h>
+#include <stdio.h>
 
 void print_name(char *name, void (*f)(char *));
 
