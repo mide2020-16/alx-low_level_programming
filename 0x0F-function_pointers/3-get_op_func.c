b80e6a93c516132446f2c67581c01e602883aaf3
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2023-11-30 01:38:13 +0100

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * get_op_func - Get the operator to perform an operaation
+ * @s: the symbol
+ * Return: Success
+ */
+
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
