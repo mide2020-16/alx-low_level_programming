0c84eeed67170ed7deed8759d0eecd7e66d0e325
mide2020-16 <efuwapeayomide51@gmail.com>
2023-12-01 02:12:30 -0800

    done with 2-print_string.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * printt_strings - Prints args as strings
+ *
+ * @separator : the comma
+ * @n: the number of args
+ * Return: Success
+ *
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list arg;
+	
+	unsigned int i;
+
+	va_start(arg, n);
+
+	for (i = 0; i < n; i++)
+	{
+		const char *str;
+		str = va_arg(arg, const char *);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(arg);
+
+}
