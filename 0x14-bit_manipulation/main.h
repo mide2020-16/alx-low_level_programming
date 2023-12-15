d1b777d6a5bf5487dcc5102c3cb53aa80e1176ca
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2023-12-15 10:44:24 +0100

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,19 +1,22 @@
-#ifndef MIAN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdlib.h>
 
-/*************************************************************************************/
-unsigned int binary_to_uint(const char *b); /* converts binary to unsigned int*/
+/**********************************************/
+unsigned int binary_to_uint(const char *b);
+/* converts binary to unsigned int*/
 
-void print_binary(unsigned long int n); /* prints the binary representation of a number */
+void print_binary(unsigned long int n);
+/* prints binary representation of a number */
 
-int get_bit(unsigned long int n, unsigned int index); /* returns the value of a bit at a given index */
+int get_bit(unsigned long int n, unsigned int index);
+/* returns bit at a given index */
 
-int set_bit(unsigned long int *n, unsigned int index); /**/
+int set_bit(unsigned long int *n, unsigned int index);
 
-int clear_bit(unsigned long int *n, unsigned int index); /**/
+int clear_bit(unsigned long int *n, unsigned int index);
 
-unsigned int flip_bits(unsigned long int n, unsigned long int m); /**/
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
 #endif /* MAIN_H */
