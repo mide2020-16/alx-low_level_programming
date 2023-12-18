b1183d0df03a0ceae02d43af37209db4b518962b
mide2020-16 <efuwapeayomide51@gmail.com>
2023-12-18 23:34:32 +0100

    new

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t append_text;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND, S_IRUSR | S_IWUSR);
+
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		append_text = write(fd, text_content, strlen(text_content));
+		if (append_text == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	else
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
