358aff852a3b65d05a376c77799f174d516b27ba
mide2020-16 <efuwapeayomide51@gmail.com>
2023-12-22 07:21:10 +0100

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-#include <sys/stat.h>
-#include <sys/types.h>
 
-#define BUF_SIZE 1024
+#define BUF_SIZE 10 * 1024
 
 /**
  * print_error - Prints error message with an exit code
@@ -15,8 +13,20 @@
 
 void print_error(int exit_code, const char *message, const char *arg)
 {
-	dprintf(STDERR_FILENO, message, arg);
+	printf(message, arg);
 	exit(exit_code);
+}
+
+/**
+ * close_fd - closes fildes
+ * @fd_from: file_from
+ * @fd_to: file to
+*/
+
+void close_fd(int fd_from, int fd_to)
+{
+	close(fd_from);
+	close(fd_to);
 }
 
 /**
@@ -28,45 +38,39 @@ void print_error(int exit_code, const char *message, const char *arg)
 
 int main(int argc, char **argv)
 {
-	const char *file_from;
-	const char *file_to;
-	int fd_from;
-	int fd_to;
+	const char *file_from, *file_to;
+	int fd_from, fd_to;
 	char buffer[BUF_SIZE];
 	ssize_t read_bytes, write_bytes;
-	mode_t permission;
 
 	file_from = argv[1];
 	file_to = argv[2];
 	if (argc != 3)
 		print_error(97, "Usage : cp file_from file_to\n", NULL);
 	fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1 || file_from == NULL)
+	if (fd_from == -1)
 		print_error(98, "Error: Can't read from file %s\n", file_from);
-	permission =  S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH; /* 0664*/
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, permission);
-	if (fd_to == -1 || file_from == NULL)
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
 		print_error(99, "Error: Can't write to %s\n", file_to);
 	read_bytes = read(fd_from, buffer, BUF_SIZE);
 	if (read_bytes == -1)
 	{
-		close(fd_from);
-		close(fd_to);
+		close_fd(fd_from, fd_to);
 		print_error(98, "Error: Can't read from file %s\n", file_from);
 	}
 	if (read_bytes > 0)
 	{
 		write_bytes = write(fd_to, buffer, read_bytes);
-		if (write_bytes != read_bytes || write_bytes == -1)
+		if (write_bytes != read_bytes)
 		{
-			close(fd_from);
-			close(fd_to);
+			close_fd(fd_from, fd_to);
 			print_error(99, "Error: Can't write to %s\n", file_to);
 		}
 	}
 	if (close(fd_from) == -1)
-		print_error(100, "Error: Can't close fd %d", file_from);
+		print_error(100, "Error: Can't close fd %d\n", file_from);
 	if (close(fd_to) == -1)
-		print_error(100, "Error: Can't close fd %d", file_to);
+		print_error(100, "Error: Can't close fd %d\n", file_to);
 	return (0);
 }
