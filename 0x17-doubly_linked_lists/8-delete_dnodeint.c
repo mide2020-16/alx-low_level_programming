68bd8120e215311193b48b81818b0fb7edbb4250
mide2020-16 <efuwapeayomide51@gmail.com>
2024-01-26 04:33:23 -0800

    error checks

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,7 +10,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
     dlistint_t *current;
 
-    if (head == NULL || index < 0)
+    if (head == NULL)
         return -1;
 
     current = *head;
