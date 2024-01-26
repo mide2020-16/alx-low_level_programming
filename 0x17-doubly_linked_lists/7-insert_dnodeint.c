ecee9adcfd856da743249b6aa8c1be51590019f5
mide2020-16 <efuwapeayomide51@gmail.com>
2024-01-26 14:45:59 +0100

    all

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,9 +17,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	current = *h;
 
-	if (idx > dlistint_len(current))
-		return (NULL);
-
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
