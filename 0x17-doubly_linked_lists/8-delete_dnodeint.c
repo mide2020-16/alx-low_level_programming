6581f92c7b5237b73bd1eed55ff5db91e880eba0
mide2020-16 <efuwapeayomide51@gmail.com>
2024-01-27 14:52:13 +0100

    del

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,23 +1,20 @@
 #include "lists.h"
 
 /**
-* delete_dnodeint_at_index - Deletes a node at a particular index
-* @head: The node to delete
-* @index: the index of the the node
-* Return: 1 on Success and -1 for Failure
-*/
+ * delete_dnodeint_at_index - Deletes a node at a particular index
+ * @head: The node to delete
+ * @index: the index of the the node
+ * Return: 1 on Success and -1 for Failure
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *current;
 	unsigned int i = 0;
 
 	if (*head == NULL)
-		return (-1);
+		return -1;
 
 	current = *head;
-
-	if (current == NULL)
-		return (-1);
 
 	if (index == 0)
 	{
@@ -25,7 +22,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		if (*head != NULL)
 			(*head)->prev = NULL;
 		free(current);
-		return (1);
+		return 1;
 	}
 
 	while (current != NULL && i < index)
@@ -33,6 +30,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		current = current->next;
 		i++;
 	}
+
+	if (current == NULL)
+		return -1;
 
 	if (current->prev != NULL)
 		current->prev->next = current->next;
@@ -43,6 +43,5 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		current->next->prev = current->prev;
 
 	free(current);
-	return (1);
+	return 1;
 }
-
