f7589714b7d138dd59f59b906d7a8e6d1b7b6d0b
mide2020-16 <efuwapeayomide51@gmail.com>
2024-01-29 10:51:18 +0100

    d

--- 0x18-dynamic_libraries/3-string_operation.c
+++ 0x18-dynamic_libraries/3-string_operation.c
@@ -1,4 +1,15 @@
 #include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - Writes a character to the standard output
+ * @c: The character to be written
+ *
+ * Return: On success, 1. On error, -1.
+ */
+int _putchar(char c) {
+    return write(1, &c, 1);
+}
 
 /**
  * _strspn - Calculates the length of the initial segment of a string
